30d985cefa1266579a276c9ac4a5ef12666b05c2
VyacheslavVolkhin <vv@htmlworker.com>
2024-02-20 13:49:38 +0300

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1,106 +1,217 @@
-$(document).ready(function(){
-
-	//phone masked
-	$('input[type="tel"]').mask("+7 (999) 999-99-99",{placeholder:"+7 (___) ___-__-__"});
-	$('input[type="tel"]').on('click', function() {
-		$(this).setCursorPosition(4);
-	})
-	$.fn.setCursorPosition = function(pos) {
-	  this.each(function(index, elem) {
-	    if (elem.setSelectionRange) {
-	      elem.setSelectionRange(pos, pos);
-	    } else if (elem.createTextRange) {
-	      var range = elem.createTextRange();
-	      range.collapse(true);
-	      range.moveEnd('character', pos);
-	      range.moveStart('character', pos);
-	      range.select();
-	    }
-	  });
-	  return this;
-	};
-
-	//popup block
-	$('.js-popup-wrap .js-btn-toggle').on('click', function() {
-		if ($(this).hasClass('active')) {
-			$(this).removeClass('active');
-			$('body').removeClass('menu-show');
-		} else {
-			$('.js-popup-wrap:not(.no-close) .js-btn-toggle').removeClass('active');
-			$(this).addClass('active');
-			if ($(this).parent().hasClass('main-menu-wrap')) {
-				$('body').addClass('menu-show');
-			}
+//js tabs
+const tabsNav = document.querySelectorAll('.js-tabs-nav')
+const tabsBlocks = document.querySelectorAll('.js-tab-block')
+function tabsActiveStart() {
+	for (iTab = 0; iTab < tabsBlocks.length; iTab++) {
+		if (tabsBlocks[iTab].classList.contains('active')) {
+			tabsBlocks[iTab].classList.remove('active')
 		}
-		return false;
-	})
-	$('.js-popup-wrap .js-btn-close').on('click', function() {
-		$(this).parents('.js-popup-wrap').children('.js-btn-toggle').removeClass('active');
-		$('body').removeClass('menu-show');
-		return false;
-	})
-	$(document).click(function(event) {
-	    if ($(event.target).closest(".js-popup-block").length) return;
-	    $('.js-popup-wrap:not(.no-close) .js-btn-toggle').removeClass('active');
-	    $('body').removeClass('menu-show');
-	    event.stopPropagation();
-	});
-	$('.js-popup-wrap').each(function() {
-		if ($(this).hasClass('js-popup-select')) {
-			// alert(1)
-			if ($(this).find('.js-popup-block').find('.active').length>0) {} else {
-				$(this).find('.js-popup-block').find('li').eq(0).children('a').addClass('active');
-			}
-			var currentSelect = $(this).find('.js-popup-block').find('.active').html();
-			$(this).find('.js-btn-toggle').html(currentSelect);
-		}
-	})
-	$('.js-popup-wrap.js-popup-select .js-popup-block a').on('click', function() {
-		if ($(this).hasClass('active')) {} else {
-			$(this).parents('.js-popup-wrap').find('.js-popup-block').find('.active').removeClass('active');
-			$(this).addClass('active');
-		}
-		$('.js-popup-wrap').each(function() {
-			if ($(this).hasClass('js-popup-select')) {
-				if ($(this).find('.js-popup-block').find('.active').length>0) {} else {
-					$(this).find('.js-popup-block').find('li').eq(0).children('a').addClass('active');
+	}
+	for (i = 0; i < tabsNav.length; i++) {
+		let tabsNavElements = tabsNav[i].querySelectorAll('[data-tab]')
+		for (iElements = 0; iElements < tabsNavElements.length; iElements++) {
+			if (tabsNavElements[iElements].classList.contains('active')) {
+				let tabsNavElementActive = tabsNavElements[iElements].dataset.tab
+				for (j = 0; j < tabsBlocks.length; j++) {
+					if (tabsBlocks[j].dataset.tab.toString().indexOf(tabsNavElementActive) > -1) {
+						console.log(tabsBlocks[j].dataset.tab.toString().indexOf(tabsNavElementActive))
+						tabsBlocks[j].classList.add('active')
+					}
 				}
-				var currentSelect = $(this).find('.js-popup-block').find('.active').html();
-				$(this).find('.js-btn-toggle').html(currentSelect);
 			}
-		})
-		$(this).parents('.js-popup-wrap').find('.js-btn-toggle').removeClass('active');
-		return false;
-	})
-
-	//tabs
-	$('.js-tabs-nav').each(function() {
-		$('.js-tab-block[data-tab*="'+$(this).find('.active').attr('data-tab')+'"]').addClass('active');
-	})
-	$('.js-tab-title').each(function() {
-		if ($(this).hasClass('active')) {
-			$(this).next('.js-tab-content').show(0);
+		}
+	}
+	
+}
+for (i = 0; i < tabsNav.length; i++) {
+	tabsNav[i].addEventListener('click', function (e) {
+		if (e.target.closest('[data-tab]')) {
+			let tabsNavElements = this.querySelector('[data-tab].active')
+			tabsNavElements ? tabsNavElements.classList.remove('active') : false
+			e.target.closest('[data-tab]').classList.add('active')
+			tabsActiveStart()
+			e.preventDefault()
+			e.stopPropagation()
+			return false
 		}
 	})
-	$('.js-tabs-nav li a').on('click', function() {
-		if ($(this).hasClass('active')) {} else {
-			$('.js-tab-block').removeClass('active');
-			$(this).parents('.js-tabs-nav').find('.active').removeClass('active');
-			$(this).addClass('active');
-			$('.js-tabs-nav').each(function() {
-				$('.js-tab-block[data-tab*="'+$(this).find('.active').attr('data-tab')+'"]').addClass('active');
+}
+tabsActiveStart()
+
+
+//js popup wrap
+const togglePopupButtons = document.querySelectorAll('.js-btn-popup-toggle')
+const closePopupButtons = document.querySelectorAll('.js-btn-popup-close')
+const popupElements = document.querySelectorAll('.js-popup-wrap')
+const wrapWidth = document.querySelector('.wrap').offsetWidth
+const bodyElem = document.querySelector('body')
+function popupElementsClear() {
+	document.body.classList.remove('menu-show')
+	document.body.classList.remove('filter-show')
+	document.body.classList.remove('search-show')
+	popupElements.forEach(element => element.classList.remove('popup-right'))
+}
+function popupElementsClose() {
+	togglePopupButtons.forEach(element => {
+		if (!element.closest('.no-close')) {
+			element.classList.remove('active')
+		}
+	})
+}
+function popupElementsContentPositionClass() {
+	popupElements.forEach(element => {
+		let pLeft = element.offsetLeft
+		let pWidth = element.querySelector('.js-popup-block').offsetWidth
+		let pMax = pLeft + pWidth;
+		if (pMax > wrapWidth) {
+			element.classList.add('popup-right')
+		} else {
+			element.classList.remove('popup-right')
+		}
+	})
+}
+for (i = 0; i < togglePopupButtons.length; i++) {
+	togglePopupButtons[i].addEventListener('click', function (e) {
+		popupElementsClear()
+		if (this.classList.contains('active')) {
+			this.classList.remove('active')
+		} else {
+			popupElementsClose()
+			this.classList.add('active')
+			if (this.closest('.popup-menu-wrap')) {
+				document.body.classList.add('menu-show')
+			}
+			if (this.closest('.popup-search-wrap')) {
+				document.body.classList.add('search-show')
+			}
+			if (this.closest('.popup-filter-wrap')) {
+				document.body.classList.add('filter-show')
+			}
+			popupElementsContentPositionClass()
+		}
+		e.preventDefault()
+		e.stopPropagation()
+		return false
+	})
+}
+for (i = 0; i < closePopupButtons.length; i++) {
+	closePopupButtons[i].addEventListener('click', function (e) {
+		popupElementsClear()
+		popupElementsClose()
+		e.preventDefault()
+		e.stopPropagation()
+		return false;
+	})
+}
+document.onclick = function (event) {
+	if (!event.target.closest('.js-popup-block')) {
+		popupElementsClear()
+		popupElementsClose()
+	}
+}
+popupElements.forEach(element => {
+	if (element.classList.contains('js-popup-select')) {
+		let popupElementSelectItem = element.querySelectorAll('.js-popup-block li a')
+		if (element.querySelector('.js-popup-block .active')) {
+			element.classList.add('select-active')
+			let popupElementActive = element.querySelector('.js-popup-block .active').innerHTML
+			let popupElementButton = element.querySelector('.js-btn-popup-toggle')
+			popupElementButton.querySelector('.button-title').innerHTML = ''
+			popupElementButton.querySelector('.button-title').insertAdjacentHTML('beforeend', popupElementActive)
+		} else {
+			element.classList.remove('select-active')
+		}
+		for (i = 0; i < popupElementSelectItem.length; i++) {
+			popupElementSelectItem[i].addEventListener('click', function (e) {
+				this.closest('.js-popup-wrap').classList.add('select-active')
+				if (this.closest('.js-popup-wrap').querySelector('.js-popup-block .active')) {
+					this.closest('.js-popup-wrap').querySelector('.js-popup-block .active').classList.remove('active')
+				}
+				this.classList.add('active')
+				let popupElementActive = element.querySelector('.js-popup-block .active').innerHTML
+				let popupElementButton = element.querySelector('.js-btn-popup-toggle')
+				popupElementButton.querySelector('.button-title').innerHTML = ''
+				popupElementButton.querySelector('.button-title').insertAdjacentHTML('beforeend', popupElementActive)
+				popupElementsClear()
+				popupElementsClose()
+				if (!this.closest('.js-tabs-nav')) {
+					e.preventDefault()
+					e.stopPropagation()
+					return false
+				}
 			})
 		}
+	}
+})
+
+$(document).ready(function(){
+
+
+	//swipebox
+	if (!!$('[data-swipebox]').offset()) {
+		$('[data-swipebox]').swipebox();
+	}
+
+	//frm counter   
+	$('.js-counter .js-button-counter-minus').on('click', function(){
+		var cnt=$(this).parents('.js-counter').find('.js-input-counter').val();
+		cnt=parseInt(cnt);
+		if (cnt>0) {
+			$(this).parents('.js-counter').find('.js-input-counter').val(cnt-1);
+		}
 		return false;
 	})
-	$('.js-tab-title').on('click' , function() {
-		if ($(this).hasClass('active')) {
-			$(this).removeClass('active').next('.js-tab-content').slideUp(200);
-		} else {
-			$(this).addClass('active').next('.js-tab-content').slideDown(200);
-		}
+	$('.js-counter .js-button-counter-plus').on('click', function(){
+		var cnt=$(this).parents('.js-counter').find('.js-input-counter').val();
+		$(this).parents('.js-counter').find('.js-input-counter').val(cnt-1+2);
+		return false;
 	})
-	
+
+	//links-slider-box
+	if (!!$('.links-slider-box').offset()) {
+		if ($(window).innerWidth() > 1023) {
+			$('.links-slider-box .slider').slick({
+				dots: false,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				touchThreshold: 100,
+				variableWidth: true,
+				infinite: false,
+				adaptiveHeight: false,
+				rows: 1,
+				swipeToSlide: true,
+				prevArrow: '<span class="btn-action-ico ico-arrow ico-arrow-prev"></span>',
+				nextArrow: '<span class="btn-action-ico ico-arrow ico-arrow-next"></span>',
+			});
+		}
+	}
+
+
+	//gallery-slider-box
+	if (!!$('.gallery-slider-box').offset()) {
+		$('.gallery-slider-box .slider').slick({
+			dots: false,
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			touchThreshold: 100,
+			variableWidth: false,
+			infinite: false,
+			adaptiveHeight: false,
+			rows: 1,
+			swipeToSlide: true,
+			autoplay: false,
+			autoplaySpeed: 5000,
+			prevArrow: false,
+			nextArrow: false,
+			responsive: [
+				{
+					breakpoint: 640,
+					settings: {
+						slidesToShow: 2,
+					}
+				},
+			]
+		});
+		
+	}
 });
-
-
